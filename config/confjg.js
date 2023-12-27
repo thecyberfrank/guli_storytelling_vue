10327a04cba80d909750429f3e14f2d415e35a61
thecyberfrank <thecyberfrank@gmail.com>
2023-12-27 18:17:43 +0800

    Initial commit

---
+++ config/confjg.js
@@ -0,0 +1,7 @@
+"use strict";
+const BASE_URL = "http://127.0.0.1:80";
+const BASE_UPLOAD_URL = "http://127.0.0.1:80";
+const WebSocket_BASE_URL = "ws://http://127.0.0.1:80/api/websocket";
+exports.BASE_UPLOAD_URL = BASE_UPLOAD_URL;
+exports.BASE_URL = BASE_URL;
+exports.WebSocket_BASE_URL = WebSocket_BASE_URL;
